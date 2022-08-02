21017cfa4190a87359ece820fd5c0a87a9a2baa9
Muhammad Rasyid Ridho <99931023+rasyid15@users.noreply.github.com>
2022-08-02 10:34:59 +0700

    Delete Latihan1.php

--- Latihan1.php
+++
@@ -1,8 +0,0 @@
-<?php 
-$nama = "Muhammad Rasyid Ridho";
-$umur = 17;
-$asalSekolah = 'SMK Telkom Malang';
-
-echo "Halo, perkenalkan namaku ".$nama.", aku berusia ".$umur." tahun. Sekarang aku bersekolah di ".$asalSekolah;
-
-?>
